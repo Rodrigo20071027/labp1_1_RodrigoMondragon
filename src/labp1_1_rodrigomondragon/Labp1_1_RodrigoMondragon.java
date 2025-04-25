96be5a03a762c3810767b29f9853b26002d481d3
Rodrigo20071027 <rodrick98n@gmail.com>
2025-04-25 15:05:23 -0600

    Commit 2 y final

--- src/labp1_1_rodrigomondragon/Labp1_1_RodrigoMondragon.java
+++ src/labp1_1_rodrigomondragon/Labp1_1_RodrigoMondragon.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package labp1_1_rodrigomondragon;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +15,43 @@ public class Labp1_1_RodrigoMondragon {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int respuesta= -1;
+        Scanner read = new Scanner(System.in);
+        while(respuesta != 0){
+            System.out.println("Menu Principal");
+            System.out.println("Opcion 1. Mensaje");
+            System.out.println("Opcion 2. Calculadora");
+            System.out.println("Opcion 3. Edades");
+            
+            respuesta = read.nextInt();
+            
+            if(respuesta == 1){
+                System.out.println("Hola soy yo");
+                
+                //opcion 2: Calculadora
+            } else if(respuesta == 2){
+                System.out.println("Ingrese numero uno: ");
+                int num1 = read.nextInt();
+                System.out.println("Ingrese numero dos: ");
+                int num2 = read.nextInt();
+                int resultado = num1*num2;
+                System.out.println("Resultado: "+resultado);
+                
+                //Opcion 3
+            } else if(respuesta ==3){
+                System.out.println("Ingrese su edad: ");
+                int edad = read.nextInt();
+                
+                if (edad >= 18){
+                    System.out.println("Usted es un mayor");
+                }else{
+                    System.out.println("Usted es un menor");
+                }
+                        
+            
+            }
+        }  
+            
     }
     
 }
