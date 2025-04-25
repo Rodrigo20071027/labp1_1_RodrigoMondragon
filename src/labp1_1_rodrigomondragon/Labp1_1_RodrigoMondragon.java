9c0048fb35c875b72598a12d9714c64fce74bbf6
Rodrigo20071027 <rodrick98n@gmail.com>
2025-04-25 14:02:17 -0600

    Commit inicial

---
+++ src/labp1_1_rodrigomondragon/Labp1_1_RodrigoMondragon.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labp1_1_rodrigomondragon;
+
+/**
+ *
+ * @author HP 15DA0086OD
+ */
+public class Labp1_1_RodrigoMondragon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
